51e91a124d0cd01f917ef30eb20f648229e68c41
zhenshi <zhenshi@ksztone.com>
2025-11-26 14:49:31 +0800

    release: 1.0.0

---
+++ main.d.ts
@@ -0,0 +1,11 @@
+declare module "remark-obsidian" {
+    import { Plugin } from "unified";
+    /**
+     * Remark plugin to parse Obsidian embedding formats:
+     * - ![[filename]] - Wikilink embedding (parsed from text nodes)
+     * - ![[link|altname]] - Wikilink embedding with alt text
+     * - ![alt](url) - Image embedding (converted from image nodes)
+     */
+    export const remarkObsidian: Plugin;
+    export default remarkObsidian;
+}
